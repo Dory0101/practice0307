c40036c4cd3722542a7f62d54413db165fb6dd32
Doreen <doreen7932@gmail.com>
2022-03-08 16:59:54 +0800

    inherit practice_student&graduatedstudent

--- lib/src/main/java/com/example/lib/GraduatedStudent.kt
+++ lib/src/main/java/com/example/lib/GraduatedStudent.kt
@@ -1,4 +1,12 @@
 package com.example.lib
 
-class GraduatedStudent {
+class GraduatedStudent constructor(name: String, english: Int, math: Int)
+    : Student(name, english, math) {
+        constructor(name: String): this(name, 0,0) {}
+        constructor(): this("", 0, 0) {}
+}
+
+fun main() {
+    val doreen = GraduatedStudent("Doreen", 80, 80)
+    doreen.print()
 }
--- lib/src/main/java/com/example/lib/Student.kt
+++ lib/src/main/java/com/example/lib/Student.kt
@@ -1,4 +1,29 @@
 package com.example.lib
 
-class Student constructor(var name: String, var english: Int, var math: Int){
+open class Student constructor(var name: String, var english: Int, var math: Int){
+    constructor(name: String): this(name, 0, 0) {}
+    constructor(): this("", 0, 0) {}
+
+    fun print() {
+        println("$name $english $math ${average()} ${highest()} ${grading()}")
+    }
+    fun average() = (english+math)/2
+
+    fun highest() = if (english > math) "english" else if (english==math)  "english = math" else "math"
+
+    fun grading() = when (average()/10) {
+            10 -> "A"
+            9 -> "A"
+            8 -> "B"
+            7 -> "C"
+            6 -> "D"
+            else -> "F"
+        }
+}
+
+fun main() {
+    val jenny = Student("Jenny", 70, 80)
+    val kui = Student("Kui")
+    jenny.print()
+    kui.print()
 }
